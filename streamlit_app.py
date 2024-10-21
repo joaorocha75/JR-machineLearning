76fbcbc8fb9d1f520890660895d293ee124551e5
João Rocha <93060408+joaorocha75@users.noreply.github.com>
2024-10-21 17:57:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ with st.expander('Data Visualization'):
 with st.sidebar:
   st.header('Input Features')
   island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  gender = st.selectbox('Gender',('male','female'))
+  sex = st.selectbox('Sex',('male','female'))
   bill_length_mm = st.slider('Bill lenght (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
@@ -40,7 +40,7 @@ with st.sidebar:
     'bill_depth_mm': bill_depth_mm,
     'flipper_length_mm': flipper_length_mm  ,
     'body_mass_g': body_mass_g,
-    'gender': gender
+    'sex': gender
   }
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
