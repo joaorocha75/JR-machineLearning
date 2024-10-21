3a5fc72abc1a8de06be7dc7734228fc8fd0ea43f
João Rocha <93060408+joaorocha75@users.noreply.github.com>
2024-10-21 15:53:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('🤖Machine Learning App')
 
 st.info('This is app, builds a machine learning model!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
